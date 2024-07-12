addb85b97f3a9330cd24f79efe7f9020b817798c
Juli <julianarueda40@gmail.com>
2024-07-12 20:52:11 -0300

    arreglo url del config

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
-var BASE_PATH = 'https://github.com/tachi123/CAC-Python-Backend-2024/';
+var BASE_PATH = 'https://tachi123.github.io/CAC-Python-Backend-2024/';
 
 var URL_WS = "https://tachi123.pythonanywhere.com/"
